a539bbc456e52c05aaaecc563497e73c3914c945
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-06-04 23:56:41 +0600

    Update Bubble sort.cpp

--- Array/Sort/Bubble sort.cpp
+++ Array/Sort/Bubble sort.cpp
@@ -1,1 +1,43 @@
 
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    int temp, flag;
+    for (int i = 0; i < size - 1; i++) {
+        flag = 0;
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                flag = 1;
+            }
+        }
+        
+        if (flag == 0) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {1, 5, 7, 2, 3, -9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, size);
+    bubbleSort(arr, size);
+
+    cout << "After bubble sort, the array is: ";
+    printArray(arr, size);
+
+    return 0;
+}
