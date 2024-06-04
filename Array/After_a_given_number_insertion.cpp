98b751851956037ea9f4e95e5f858d71499cd557
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-06-04 22:44:49 +0600

    Add files via upload

---
+++ Array/After_a_given_number_insertion.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+void After_insert(int arr[],int n,int value,int num){
+    cout<<"Before the insertion"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    n++;
+    int pos=-1;
+    for(int i=0;i<n;i++){
+    if(arr[i]==num){
+        pos=i;
+        break;
+      }
+     }
+     if(pos==-1){
+        cout<<"the number is not foun in this array"<<endl;
+        return ;
+     }
+    for(int i=n-1;i>pos;i--){
+        arr[i]=arr[i-1];
+
+     }
+     arr[pos+1]=value;
+
+
+     cout<<"\nAfter the insertion"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
+int main(){
+    int arr[]={2,3,4,5,7};
+    int  n=5, value, num;
+    cout<<"Enter the number which after insertion"<<endl;
+    cin>>num;
+    cout<<"Enter the insert value "<<endl;
+    cin>>value;
+
+    After_insert( arr, n, value, num);
+
+}
