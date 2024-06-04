303897b1e77742eb66b0e0c3e64793b391905dd4
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-06-04 21:40:16 +0600

    Update First_position_insertion.cpp

--- Array/First_position_insertion.cpp
+++ Array/First_position_insertion.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
+void first_insertion(int arr[],int value,int n,int pos){
+    
+    cout<<"After the insertion:"<<endl;
+    for(int i=0;i<n;i++){
+      cout<<arr[i]<<" ";
+    }
+    for (int i = n - 1; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+     arr[pos - 1] = value;
+     cout<<"\nAfter the insertion:";
+     for(int i=0;i<n;i++){
+      cout<<arr[i]<<" ";
+    }
 
+
+
+}
+int main(){
+    int value,n=5,pos=1;
+    int arr[]={2,3,4,5,7};
+    cout<<" Enter the value ";
+    cin>>value;
+    
+    first_insertion(arr, value,n,pos);
+
+}
