42acb3f35c5b102687ff41ad40f5f041a9f14c35
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-06-09 11:33:38 +0600

    Update Begin insert in Linked list.cpp

--- Linked_list/Begin insert in Linked list.cpp
+++ Linked_list/Begin insert in Linked list.cpp
@@ -1,36 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-struct node {
+typedef struct node {
     int data;
     struct node *next;
-};
+} node;
 
-int main() {
-    struct node *head = NULL;
-    struct node *new_node;
-
-    printf("Enter the data of 5 nodes:\n");
-
-    for (int i = 0; i < 5; i++) {
-        new_node = (struct node*)malloc(sizeof(struct node));
-        if (new_node == NULL) {
-            printf("Memory allocation failed\n");
-            return 1;
-        }
-        scanf("%d", &new_node->data);
-        new_node->next = head;
-        head = new_node;
+void insert(node *s, int data) {
+    while (s->next != NULL) {
+        s = s->next;
     }
-
-    
-    struct node *current = head;
-    while (current != NULL) {
-        printf("%d -> ", current->data);
-        current = current->next;
+    s->next = (node*)malloc(sizeof(node));
+    s->next->next = NULL;
+    s->next->data = data;
+}
+void display(node *s) {
+    while (s->next != NULL) {
+        printf("%d->", s->next->data);
+        s = s->next;
     }
     printf("NULL\n");
+}
+void begin_insert(node *s, int item) {
+    node *new_node = (node*)malloc(sizeof(node));
+    new_node->data = item;
+    new_node->next = s->next;
+    s->next = new_node;
+}
 
+int main() {
+    node *first = (node*)malloc(sizeof(node));
+    first->next = NULL;
     
+    insert(first, 3);
+    insert(first, 5);
+    insert(first, 7);
+    insert(first, 8);
+    
+    printf("Before the insertion, the list is:\n");
+    display(first);
+    
+    int item;
+    printf("Enter the item to insert at the beginning: ");
+    scanf("%d", &item);
+    
+    begin_insert(first, item);
+    printf("After the insertion, the list is:\n");
+    display(first);
     return 0;
 }
