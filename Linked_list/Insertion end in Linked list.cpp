4964477f657c77c6b506d14b5ac9852f044030c1
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-06-09 12:13:38 +0600

    Update Insertion end in Linked list.cpp

--- Linked_list/Insertion end in Linked list.cpp
+++ Linked_list/Insertion end in Linked list.cpp
@@ -1,43 +1,56 @@
-
-#include<iostream>
-#include<stdlib.h>
-using namespace std;
-struct node{
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node {
     int data;
     struct node *next;
+} node;
 
-};
-int main(){
-    struct node *head=0;
-    struct node *new_node;
-     struct node *temp;
-
-    cout<<"Enter the value of 5 node"<<endl;
-    for(int i=0;i<5;i++){
-        new_node=new node;
-        cin>>new_node->data;
-        new_node->next=head;
-        head=new_node;
+void insert(node *s, int data) {
+    while (s->next != NULL) {
+        s = s->next;
     }
-     new_node=new node;
-    cout<<"Enter which node data was insert"<<endl;
-    cin>>new_node->data;
-    new_node->next=NULL;
-    
-    temp=head;
+    s->next = (node*)malloc(sizeof(node));
+    s->next->next = NULL;
+    s->next->data = data;
+}
+void display(node *s) {
+    while (s->next != NULL) {
+        printf("%d->", s->next->data);
+        s = s->next;
+    }
+    printf("NULL\n");
+}
+void End_insert(node *s, int item) {
+    node *new_node = (node*)malloc(sizeof(node));
+    new_node->data = item;
+
+    node* temp=s;
     while(temp->next!=NULL){
-        temp=temp->next;
+      temp=temp->next;
     }
     temp->next=new_node;
-    temp=new_node;
+    new_node->next=NULL;
 
+}
 
-   struct node * current=head;
-   while(current!=NULL){
-   
-    cout<<"->"<<current->data;
-     current=current->next;
-   }
-
-
+int main() {
+    node *first = (node*)malloc(sizeof(node));
+    first->next = NULL;
+    
+    insert(first, 3);
+    insert(first, 5);
+    insert(first, 7);
+    insert(first, 8);
+    
+    printf("Before the insertion, the list is:\n");
+    display(first);
+    
+    int item;
+    printf("Enter the item to insert at the End: ");
+    scanf("%d", &item);
+    
+    End_insert(first, item);
+    printf("After the insertion, the list is:\n");
+    display(first);
+    return 0;
 }
