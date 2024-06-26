2e86f681cc2bff9cc212d37c6582c31596be1885
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-06-26 13:43:35 +0600

    Update search-in linked_list.cpp

--- Linked_list/search-in linked_list.cpp
+++ Linked_list/search-in linked_list.cpp
@@ -1,55 +1,70 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct mylist {
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
     int data;
-    struct mylist *next;
-} node;
-
-void insert(node *s, int data) {
-    while (s->next != NULL) {
-        s = s->next;
+    struct node* next;
+}node;
+void insert(node *s,int data){
+    while(s->next!=NULL){
+        s=s->next;
     }
-    s->next = (node*)malloc(sizeof(node));
-    s->next->data = data;
-    s->next->next = NULL;
-}
+    s->next=(node*)malloc(sizeof(node));
+    s->next->next=NULL;
+    s->next->data=data;
 
-void display(node *s) {
-    while (s->next != NULL) {
-        printf("%d\n", s->next->data);
-        s = s->next;
-    }
 }
+void display(node* s){
+   while(s->next!=NULL){
+   printf("%d->",s->next->data);
+   s=s->next;
+   }
+   printf("NULL");
 
-void search(node *s, int data) {
-    int count = 0;
-    while (s->next != NULL) {
-        if (s->next->data == data) {
-            count++;
+}
+int  search(node*s,int item){
+    int index=0;
+    while(s->next!=NULL){
+        if(s->next->data==item){
+           return index;
         }
-        s = s->next;
+        s=s->next;
+        index++;
     }
-    printf("Total %d result(s) found.\n", count);
+    return -1;
 }
+void Begin_insert(node*s,int data){
+    node* new_node=(node*)malloc(sizeof(node));
+    new_node->data=data;
+    new_node->next=s;
+    s=new_node;
+}
+int main(){
+    node *first=(node*)malloc(sizeof(node));
+    first->next=NULL;
 
-int main() {
-    node *first = (node*)malloc(sizeof(node));
-    first->next = NULL;
-    int item;
-
-    insert(first, 9);
-    insert(first, 5);
-    insert(first, 11);
-    insert(first, 4);
-    insert(first, 5); 
-
-    printf("Original list:\n");
+    insert(first,30);
+    insert(first,40);
+    insert(first,50);
+    insert(first,60);
+    
+    printf("Before the insertion the list is:\n");
     display(first);
 
-    printf("Enter the search item: ");
-    scanf("%d", &item);
-    search(first, item);
-return 0;
+    Begin_insert(first,45);
+
+    printf("\nAfter the insertion the list is:\n");
+    display(first);
+
+    search(first,60);
+
+    int index = search(first, 60);
+    if (index != -1) {
+        printf("\nItem 60 found at index: %d\n", index);
+    } else {
+        printf("\nItem 60 not found\n");
+    }
+
+
+
 }
