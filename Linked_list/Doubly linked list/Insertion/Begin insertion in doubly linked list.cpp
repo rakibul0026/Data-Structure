cdb908bf9e234debf3f6fdb0a7a6fef2b8d14ed2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-04 12:58:06 +0600

    Update Begin insertion in doubly linked list.cpp

--- Linked_list/Doubly linked list/Insertion/Begin insertion in doubly linked list.cpp
+++ Linked_list/Doubly linked list/Insertion/Begin insertion in doubly linked list.cpp
@@ -1,1 +1,69 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    struct node *prev;
+    int data;
+    struct node *next;
+} node;
+
+struct node* head = NULL;
+
+void insert(int data) {
+    struct node* temp = (node*)malloc(sizeof(node));
+    temp->prev = NULL;
+    temp->data = data;
+    temp->next = NULL;
+
+    if (head == NULL) {
+        head = temp;
+    } else {
+        struct node* current = head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = temp;
+        temp->prev = current;
+    }
+}
+
+void display() {
+    struct node* current = head;
+    while (current != NULL) {
+        printf("%d->", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void begin_insert(int item) {
+    node *new_node = (node*)malloc(sizeof(node));
+    new_node->prev = NULL;
+    new_node->data = item;
+    new_node->next = head;
+
+    if (head != NULL) {
+        head->prev = new_node;
+    }
+    head = new_node;
+}
+
+int main() {
+    insert(3);
+    insert(5);
+    insert(7);
+    insert(8);
+
+    printf("Before the insertion, the list is:\n");
+    display();
+
+    int item;
+    printf("Enter the item to insert at the beginning: ");
+    scanf("%d", &item);
+
+    begin_insert(item);
+    printf("After the insertion, the list is:\n");
+    display();
+    return 0;
+}
