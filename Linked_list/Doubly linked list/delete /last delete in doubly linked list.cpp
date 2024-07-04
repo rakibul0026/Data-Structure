cc7d85eda5df2ea92297887ad0c361e84f3fd1e0
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-05 01:33:12 +0600

    Update last delete in doubly linked list.cpp

--- Linked_list/Doubly linked list/delete /last delete in doubly linked list.cpp
+++ Linked_list/Doubly linked list/delete /last delete in doubly linked list.cpp
@@ -1,1 +1,78 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node* next;
+    struct node* prev;
+} node;
+node* head = NULL;
+
+void insert(int data) {
+    node* new_node = (node*)malloc(sizeof(node));
+    new_node->data = data;
+    new_node->next = NULL;
+
+    if (head == NULL) {
+        new_node->prev = NULL;
+        head = new_node;
+        return;
+    }
+    node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = new_node;
+    new_node->prev = temp;
+}
+void display() {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    node* temp = head;
+    while (temp != NULL) {
+        printf("%d->", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+void lastnode_delete() {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    node* temp = head;
+    if (temp->next == NULL) {
+        free(head);
+        head = NULL;
+        return;
+    }
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->prev->next = NULL;
+    free(temp);
+}
+
+int main() {
+    insert(52);
+    insert(63);
+    insert(78);
+    insert(80);
+    insert(26);
+
+    printf("\nBefore the deletion\n");
+    display();
+    
+    lastnode_delete();
+    printf("\nAfter lastnode_delete\n");
+    display();
+
+    return 0;
+}
