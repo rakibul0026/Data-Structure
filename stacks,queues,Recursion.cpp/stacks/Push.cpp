3bb1f7028b14baae13fa0645872acc1c834f86fa
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-09 09:01:31 +0600

    Update Push.cpp

--- stacks,queues,Recursion.cpp/stacks/Push.cpp
+++ stacks,queues,Recursion.cpp/stacks/Push.cpp
@@ -1,1 +1,40 @@
 
+
+#include <iostream>
+using namespace std;
+
+void push(int item, int stack[], int &top, int Maxstk) {
+    if (top == Maxstk - 1) {
+        cout << "Overflow\n";
+    } else {
+        top++;
+        stack[top] = item;
+    }
+}
+
+void display(int stack[], int top) {
+    if (top == -1) {
+        cout << "Stack is Empty\n";
+    } else {
+        for (int i = 0; i <= top; i++) {
+            cout << stack[i] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    const int Maxstk = 100; 
+    int stack[Maxstk]; 
+    int top = -1;      
+    int item;
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Enter item " << (i + 1) << " to be inserted into the stack: ";
+        cin >> item;
+        push(item, stack, top, Maxstk);
+    }
+    
+    display(stack, top);  
+     return 0;
+}
