fbe65fa0656216842943af722138fb11ce9dc793
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-10 09:38:57 +0600

    Update Fibbonacci.cpp

--- stacks,queues,Recursion.cpp/Recursion/Fibbonacci.cpp
+++ stacks,queues,Recursion.cpp/Recursion/Fibbonacci.cpp
@@ -1,1 +1,24 @@
 
+#include<iostream>
+using namespace std;
+int fib(int n){
+    if(n == 0)
+        return 0;
+    else if(n == 1)
+        return 1;
+    else
+        return (fib(n-1) + fib(n-2));
+}
+
+int main(){
+    int N,i;
+    cout<<"Enter the number of term:"<<endl;
+    cin >> N;
+    for(i=0;i<N;i++){
+        cout<<fib(i)<<" ";
+    }
+    int result = fib(N);
+    cout << result << endl;
+}
+
+
