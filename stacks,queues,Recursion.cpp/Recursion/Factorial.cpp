39b3faf4fda63ee60f0d16989d4bb7e61cbdfed7
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-10 09:37:46 +0600

    Update Factorial.cpp

--- stacks,queues,Recursion.cpp/Recursion/Factorial.cpp
+++ stacks,queues,Recursion.cpp/Recursion/Factorial.cpp
@@ -1,1 +1,18 @@
 
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if(n == 0||n==1)
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+
+int main(){
+    int N;
+    cin>>N;
+   int result = fact(N);
+   cout << result << endl;
+}
+
