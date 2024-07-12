9bfd7c000c05ee496a2155d455323df5954f6fae
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-12 09:33:02 +0600

    Update queue insert implement in Linked list.cpp

--- stacks,queues,Recursion.cpp/QUEUES/queue insert implement in Linked list.cpp
+++ stacks,queues,Recursion.cpp/QUEUES/queue insert implement in Linked list.cpp
@@ -1,1 +1,43 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node{
+    int data;
+    struct node*next;
+
+}node;
+node *front=NULL;
+node* rear=NULL;
+
+void enqueue(int data){
+   node* new_node=(node*)malloc(sizeof(node));
+   new_node->data=data;
+  
+   if(front==NULL){
+    front=rear=new_node;
+   }else
+   rear->next=new_node;
+   rear=new_node;
+   new_node->next=NULL;
+}
+
+void display(){
+    if(front==NULL){
+        printf("Queue is Empty");
+        return ;
+    }
+    node *temp=front;
+    while(temp!=NULL){
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL");
+}
+int main(){
+    enqueue(1);
+    enqueue(3);
+    enqueue(5);
+    enqueue(7);
+    display();
+
+}
