791039272a5b1df3e81c2470e787a6053a1caa07
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-12 08:52:38 +0600

    Update Queue Delete using array .cpp

--- stacks,queues,Recursion.cpp/QUEUES/Queue Delete using array .cpp
+++ stacks,queues,Recursion.cpp/QUEUES/Queue Delete using array .cpp
@@ -1,14 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
-using namespace std;
-
-const int n = 100; 
+const int n = 100;
 int queue[n];
 int front = -1, rear = -1;
 
 void enqueue(int x) {
     if (rear == n - 1) {
-        cout << "Overflow";
+        printf("Overflow\n");
     } else if (front == -1 && rear == -1) {
         front = rear = 0;
         queue[rear] = x;
@@ -20,23 +19,24 @@ void enqueue(int x) {
 
 void dequeue() {
     if (front == -1 && rear == -1) {
-        cout << "Queue is empty";
+        printf("Queue is empty\n");
     } else if (front == rear) {
-        cout << queue[front];
+        printf("%d\n", queue[front]);
         front = rear = -1;
     } else {
-        cout << queue[front];
+        printf("%d\n", queue[front]);
         front++;
     }
 }
 
 void display() {
     if (front == -1 && rear == -1) {
-        cout << "Queue is empty";
+        printf("Queue is empty\n");
     } else {
         for (int i = front; i <= rear; i++) {
-            cout << queue[i] << " ";
+            printf("%d ", queue[i]);
         }
+        printf("\n");
     }
 }
 
@@ -45,7 +45,9 @@ int main() {
     enqueue(2);
     enqueue(3);
     display();
+    
     dequeue();
     display();
+    
     return 0;
 }
