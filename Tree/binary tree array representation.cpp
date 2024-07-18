1e6095aeaa3ea909720827b4f7b604376fae5b78
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-18 16:28:54 +0600

    Update binary tree array representation.cpp

--- Tree/binary tree array representation.cpp
+++ Tree/binary tree array representation.cpp
@@ -1,1 +1,49 @@
+#include <stdio.h>
 
+char tree[10];
+
+int root(char key) {
+    if (tree[0] != '\0')
+        printf("Tree already had root");
+    else
+        tree[0] = key;
+    return 0;
+}
+
+int set_left(char key, int parent) {
+    if (tree[parent] == '\0')
+        printf("\nCan't set child at %d, no parent found", (parent * 2) + 1);
+    else
+        tree[(parent * 2) + 1] = key;
+    return 0;
+}
+
+int set_right(char key, int parent) {
+    if (tree[parent] == '\0')
+        printf("\nCan't set child at %d, no parent found", (parent * 2) + 2);
+    else
+        tree[(parent * 2) + 2] = key;
+    return 0;
+}
+
+int print_tree() {
+    printf("\n");
+    for (int i = 0; i < 10; i++) {
+        if (tree[i] != '\0')
+            printf("%c", tree[i]);
+        else
+            printf("-");
+    }
+    return 0;
+}
+
+int main() {
+    root('A');
+    set_left('B', 0);
+    set_right('C', 0);
+    set_left('D', 1);
+    set_right('E', 1);
+    set_right('F', 2);
+    print_tree();
+    return 0;
+}
