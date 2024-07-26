a2d8395a14a4574d575b55b58e4c55b0c605c50b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-26 12:57:07 +0600

    Update Insertion in BST.cpp

--- Tree/Binary search Tree(BST)/Insertion in BST.cpp
+++ Tree/Binary search Tree(BST)/Insertion in BST.cpp
@@ -1,1 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+} node;
+struct node* create(int data) {
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
 
+struct node*  Insert(struct node* node, int data) {
+    if (node == NULL) 
+        return create(data);
+
+    if (data < node->data) 
+        node->left = Insert(node->left, data);
+    else if (data > node->data) 
+        node->right = Insert(node->right, data);
+
+    return node;
+}
+
+void Inorder(node* root){
+    if(root==NULL){
+        return ;
+    }
+    Inorder(root->left);
+    printf("%d ",root->data);
+    Inorder(root->right);
+    
+
+}
+struct node *FindMin(node *root)
+{
+    while (root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root;
+}
+
+int main(){
+    node* root=NULL;
+    root=Insert(root,3);
+    root=Insert(root,4);
+    root=Insert(root,5);
+    root=Insert(root,6);
+    root=Insert(root,9);
+
+    Inorder(root);
+ 
+}
