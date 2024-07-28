f35b09ba95b44c7f96de7cf119fc81d1151a46c5
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-07-29 04:57:17 +0600

    Update Iterative Search in a Binary Search Tree.c

--- Tree/Binary search Tree(BST)/Iterative Search in a Binary Search Tree.c
+++ Tree/Binary search Tree(BST)/Iterative Search in a Binary Search Tree.c
@@ -1,1 +1,78 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+} node;
+
+struct node* create(int data) {
+    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+struct node* Insert(struct node* node, int data) {
+    if (node == NULL)
+        return create(data);
+
+    if (data < node->data)
+        node->left = Insert(node->left, data);
+    else if (data > node->data)
+        node->right = Insert(node->right, data);
+
+    return node;
+}
+
+struct node* searchIter(struct node* root, int data) {
+    while (root != NULL) {
+        if (root->data == data)
+            return root;
+
+        if (data < root->data)
+            root = root->left;
+        else
+            root = root->right;
+    }
+    return NULL;
+}
+
+void Inorder(struct node* root) {
+    if (root == NULL)
+        return;
+
+    Inorder(root->left);
+    printf("%d ", root->data);
+    Inorder(root->right);
+}
+
+struct node* FindMin(struct node* root) {
+    while (root->left != NULL)
+        root = root->left;
+
+    return root;
+}
+
+int main() {
+    node* root = NULL;
+    root = Insert(root, 3);
+    root = Insert(root, 4);
+    root = Insert(root, 5);
+    root = Insert(root, 6);
+    root = Insert(root, 9);
+
+    printf("Inorder traversal: ");
+    Inorder(root);
+
+    struct node* found = searchIter(root, 4);
+    if (found != NULL)
+        printf("\nFound 4\n");
+    else
+        printf("\n4 not found\n");
+
+    return 0;
+}
