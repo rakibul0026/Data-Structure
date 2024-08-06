47a9fe0dc206eb162e83b2f5cd5eaf0f9c24cd6f
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-06 07:46:11 +0600

    Update Linear_search.cpp

--- Array/Search in array/Linear_search.cpp
+++ Array/Search in array/Linear_search.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int linearSearch(int arr[], int size, int item) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == item) {
+            return i;  // Return the index of the found item
+        }
+    }
+    return -1;  // Return -1 if the item is not found
+}
+
+int main() {
+    int arr[] = {2, 4, 5, 6, 7, 8};
+    int size = 6;
+    int item = 5;
+
+    int result = linearSearch(arr, size, item);
+    if (result != -1) {
+        cout << "The item index is: " << result << endl;
+    } else {
+        cout << "The item is not an element in this array" << endl;
+    }
+
+    return 0;
+}
