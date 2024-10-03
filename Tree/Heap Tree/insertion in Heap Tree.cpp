0acd2f0d98f09f14ed5859421772d1595d1c23ba
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-03 10:16:18 +0600

    Update insertion in Heap Tree.cpp

--- Tree/Heap Tree/insertion in Heap Tree.cpp
+++ Tree/Heap Tree/insertion in Heap Tree.cpp
@@ -1,1 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MaxHeap
+{
+    int *arr;
+    int size;       // total elements in heap
+    int total_size; // total size of array
+
+public:
+    MaxHeap(int n)
+    {
+        arr = new int[n];
+        size = 0;
+        total_size = n;
+    }
+
+    void insert(int value)
+    {
+        if (size == total_size)
+        {
+            cout << "Heap Overflow\n";
+            return;
+        }
+        arr[size] = value;
+        int index = size;
+        size++;
+
+        // Fix the max heap property if it is violated
+        while (index > 0 && arr[(index - 1) / 2] < arr[index])
+        {
+            swap(arr[index], arr[(index - 1) / 2]);
+            index = (index - 1) / 2;
+        }
+
+        cout << value << " is inserted into the heap" << endl;
+    }
+    void print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    MaxHeap H1(6);
+    H1.insert(4);
+    H1.insert(14);
+    H1.insert(11);
+    H1.insert(12);
+
+    H1.print();
+    return 0;
+}
 
