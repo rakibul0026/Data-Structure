de98e622dc66dc5d85a4af3f5b60f98a787a8a89
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-04 07:44:25 +0600

    Update Heap Sort(Max _Heap).cpp

--- Sorting/Heap Sort/Heap Sort(Max _Heap).cpp
+++ Sorting/Heap Sort/Heap Sort(Max _Heap).cpp
@@ -1,1 +1,52 @@
+//Max Heap 
+//complexity ->0(n)
+#include <bits/stdc++.h>
+using namespace std;
+
+void Heapify(int arr[], int index, int n)
+{
+    int largest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != index)
+    {
+        swap(arr[largest], arr[index]);
+        Heapify(arr, largest, n);
+    }
+}
+
+void BuildMaxHeap(int arr[], int n)
+{
+    // Step down
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        Heapify(arr, i, n);
+    }
+}
+
+void printHeap(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {10, 3, 8, 9, 13, 18, 14, 11, 70};
+   // int n = sizeof(arr) / sizeof(arr[0]);
+
+    BuildMaxHeap(arr,9);
+    printHeap(arr, 9);
+
+    return 0;
+}
 
