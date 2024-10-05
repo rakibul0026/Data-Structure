2a982acf01c6e27135a743318e9d29e8d1ef23e5
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-05 09:28:24 +0600

    Update counting sort.cpp

--- Sorting/counting sort.cpp
+++ Sorting/counting sort.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
+void countsort(int arr[], int n)
+{
+    int i;
+    int k = arr[0];
+    for (i = 0; i < n; i++)
+    {
+        k = max(k, arr[i]);
+    }
+    int count[10] = {0};
+    for (i = 0; i < n; i++)
+    {
+        count[arr[i]]++;
+    }
+    for (i = 1; i <= k; i++)
+    {
+        count[i] += count[i - 1];
+    }
+    int output[n];
+    for (i = n - 1; i >= 0; i--)
+    {
+        output[--count[arr[i]]] = arr[i];
+    }
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = output[i];
+    }
+}
+int main()
+{
+    int arr[] = {1, 3, 2, 3, 4, 1, 6, 4, 3};
 
+    countsort(arr, 9);
+    for (int i = 0; i < 9; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
