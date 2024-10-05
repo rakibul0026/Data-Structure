d32dac22d52aa77f40dc0c34f5f5fc6be393a8e1
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-05 08:58:39 +0600

    Update Insertion Sort.cpp

--- Sorting/Insertion Sort.cpp
+++ Sorting/Insertion Sort.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
+void insertionsort(int arr[], int size)
+{
+    int temp, j;
+    for (int i = 1; i < size; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+int main()
+{
+    int arr[] = {6, 8, 9, 2, -9};
+    int n;
+    n = 5;
+    cout << "Before the insertion the array is:  ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
+    insertionsort(arr, n);
+    cout << "\nAfter the insertion the array is:  ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
