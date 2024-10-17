2b67614933c792c45f1231e82e5a65ece10d5991
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-17 10:50:11 +0600

    Update Adjacency_Matrix.c

--- Graph/Adjacency_Matrix.c
+++ Graph/Adjacency_Matrix.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
 
+int adj[100][100];
+
+int main()
+{
+
+    int node, edge;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &node);
+    printf("Enter the number of edges: ");
+    scanf("%d", &edge);
+
+    int n1, n2;
+
+    for (int i = 0; i < edge; ++i)
+    {
+        scanf("%d %d", &n1, &n2);
+
+        adj[n1][n2] = 1;
+
+        adj[n2][n1] = 1;
+    }
+
+    for (int i = 0; i < node; i++)
+    {
+        for (int j = 0; j < node; j++)
+        {
+            printf("%d\t", adj[i][j]);
+        }
+        printf("\n");
+    }
+}
