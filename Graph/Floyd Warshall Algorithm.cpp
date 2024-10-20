55ff510ae7a1d10b112b4af81953db2e33ec98a4
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-21 00:34:45 +0600

    Update Floyd Warshall Algorithm.cpp

--- Graph/Floyd Warshall Algorithm.cpp
+++ Graph/Floyd Warshall Algorithm.cpp
@@ -3,3 +3,45 @@
 To find all pair shortest path
 i.e for every u,v graph g, find the shortest path from u to v.
 */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define V 4
+#define INF 99999
+
+void printSolution(int dist[][V]);
+
+void floydWarshall(int dist[][V]) {
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] > dist[i][k] + dist[k][j] && dist[k][j] != INF && dist[i][k] != INF) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    printSolution(dist);
+}
+
+void printSolution(int dist[][V]) {
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << "   ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int graph[V][V] = { { 0, 5, INF, 10 },
+                        { INF, 0, 3, INF },
+                        { INF, INF, 0, 1 },
+                        { INF, INF, INF, 0 } };
+
+    floydWarshall(graph);
+    return 0;
+}
