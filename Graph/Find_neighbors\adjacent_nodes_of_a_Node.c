50d031483282704ea3dcd7cadc023246aa0ffbc3
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-22 01:20:06 +0600

    Update Find_neighbors\adjacent_nodes_of_a_Node.c

--- Graph/Find_neighbors\adjacent_nodes_of_a_Node.c
+++ Graph/Find_neighbors\adjacent_nodes_of_a_Node.c
@@ -1,1 +1,43 @@
-p
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int adj[100][100]; 
+
+int main() {
+    int node, edge;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &node);
+    printf("Enter the number of edges: ");
+    scanf("%d", &edge);
+
+    int n1, n2;
+
+    for (int i = 0; i < edge; ++i) {
+        scanf("%d %d", &n1, &n2);
+        adj[n1][n2] = 1; 
+        adj[n2][n1] = 1; 
+    }
+
+    printf("\nAdjacency Matrix:\n");
+    for (int i = 0; i < node; i++) {
+        for (int j = 0; j < node; j++) {
+            printf("%d\t", adj[i][j]);
+        }
+        printf("\n");
+    }
+
+    int searchNode;
+    printf("\nEnter the node to find its neighbors: ");
+    scanf("%d", &searchNode);
+
+    printf("\nNeighbors/Adjacent nodes of node %d:\n", searchNode);
+    for (int i = 0; i < node; i++) {
+        if (adj[searchNode][i] == 1) {
+            printf("%d ", i); 
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
