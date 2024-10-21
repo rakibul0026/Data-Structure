18629fa6df977695b5be0ac913e3cfb9fb91e78c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-22 00:48:52 +0600

    Update BFS algorithm.cpp

--- Graph/BFS algorithm.cpp
+++ Graph/BFS algorithm.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+void bfs(vector<vector<int>>& adj, int s) {
+    queue<int> q;  
+    vector<bool> visited(adj.size(), false);
+    visited[s] = true;
+    q.push(s);
+
+    while (!q.empty()) {
+        int curr = q.front();
+        q.pop();
+        cout << curr << " ";
+
+        for (int x : adj[curr]) {
+            if (!visited[x]) {
+                visited[x] = true;
+                q.push(x);
+            }
+        }
+    }
+}
+
+void addEdge(vector<vector<int>>& adj, int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+int main() {
+    int V;
+    cout << "Enter the number of vertices: ";
+    cin >> V;
+    vector<vector<int>> adj(V);
+
+    int edges;
+    cout << "Enter the number of edges: ";
+    cin >> edges;
+    cout << "Enter the edges (u v):\n";
+    for (int i = 0; i < edges; ++i) {
+        int u, v;
+        cin >> u >> v;
+        addEdge(adj, u, v);
+    }
+
+    int startVertex;
+    cout << "Enter the starting vertex for BFS: ";
+    cin >> startVertex;
+
+    cout << "BFS starting from " << startVertex << ":\n";
+    bfs(adj, startVertex);
+
+    return 0;
+}
 
