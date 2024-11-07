159a64a9ab2ef080ca8d857654287dca0b189ae6
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-07 20:52:22 +0600

    Update 1.Adjacency_List.c

--- Graph/1.Adjacency_List.c
+++ Graph/1.Adjacency_List.c
@@ -1,1 +1,32 @@
+/*
+  Adjacency list
+ time
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj[100]; 
+
+int main()
+{
+    int node, edge;
+    cin >> node >> edge;
+
+    for (int i = 0; i < edge; i++)
+    {
+        int n1, n2;
+        cin >> n1 >> n2;
+        adj[n1].push_back(n2);
+        adj[n2].push_back(n1); 
+    }
+
+    for (int i = 0; i < node; i++)
+    {
+        cout << "Node " << i << ": ";
+        for (int j : adj[i])
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
 
